901b7497ec2ec3bb34e53e07c3b9e0d30e101a49
Elliot Huffman <thelich2@gmail.com>
2017-11-15 17:50:49 -0500

    Drawer framework upgrade
    Add example content to the body of the doc for a visual refrence.
    Finish Temporary drawer.
    Add error checking.
    Add framework for more drawer types.
    Add initial build of a Persistent drawer

--- src/js/ELJUMP.js
+++ src/js/ELJUMP.js
@@ -2,33 +2,79 @@
 mdc.autoInit();
 
 class NavDrawer {
-    constructor(titleText, ID) {
-      this.titleText = titleText;
+    constructor(TitleText, ID, Type) {
+      this.TitleText = TitleText;
       this.ID = ID;
-      this.object = null;
+      this.Object = null;
       this.MDCDrawer = null;
+      if (typeof(Type) === "undefined") {
+        this.Type = "Temporary";
+      } else {
+        this.Type = Type;
+      }
     };
 
-    // Creates the temporary nav drawer.
+    // Creates the Nav Drawer.
     Create() {
-        var $aside = $('<aside />', {id: this.ID.toString(), "class": "mdc-temporary-drawer"});
-        let $navHeader = $("<nav>", {"class": "mdc-temporary-drawer__drawer"});
-        let $header = $("<header>", {"class": "mdc-temporary-drawer__header"});
-        let $headerContent = $("<div>", {"class": "mdc-temporary-drawer__header-content"}).text(this.titleText.toString());
-        let $navMenu = $("<nav>", {"class": "mdc-temporary-drawer__content mdc-list", id: this.ID.toString() + "-nav-menu"});
+        if (this.Object === null) {
+            // Checks which version of a nav needs to be created.
+            switch (this.Type.toString()) {
+                default:
+                    this.Type = "Temporary";
+                // The default is the temporary drawer.
+                case "Temporary":
+                    var $aside = $('<aside>', {id: this.ID.toString(), "class": "mdc-temporary-drawer"});
+                    var $navTopLvL = $("<nav>", {"class": "mdc-temporary-drawer__drawer"});
+                    var $header = $("<header>", {"class": "mdc-temporary-drawer__header mdc-theme--primary-bg mdc-theme--text-primary-on-primary"});
+                    var $headerContent = $("<div>", {"class": "mdc-temporary-drawer__header-content"}).text(this.TitleText.toString());
+                    var $navMenu = $("<nav>", {"class": "mdc-temporary-drawer__content mdc-list", id: this.ID.toString() + "-nav-menu"});
+        
+                    $('body').prepend($aside.append($navTopLvL.append($header.append($headerContent), $navMenu)));
+                    this.MDCDrawer = new mdc.drawer.MDCTemporaryDrawer($('#' + this.ID.toString())[0]);
+            
+                    this.Object = $aside;
+                    break;
+                case "Persistent":
+                    var $aside = $('<aside>', {id: this.ID.toString(), "class": "mdc-persistent-drawer"});
+                    var $navTopLvL = $("<nav>", {"class": "mdc-persistent-drawer__drawer"});
+                    var $spacer = $('<div>', {"class": "mdc-persistent-drawer__toolbar-spacer"});
+                    var $listGroup = $("<div>", {"class": "mdc-list-group"});
+                    var $listContent = $("<nav>", {"class": "mdc-list"});
+                    var $listDivider = $("<hr>", {"class": "mdc-list-divider"});
+                    var $listContent2 = $("<nav>", {"class": "mdc-list"});
 
-        $header.append($headerContent);
-        $('body').prepend($aside.append($navHeader, $header, $navMenu));
-        this.MDCDrawer = new mdc.drawer.MDCTemporaryDrawer($('#' + this.ID.toString())[0]);
+                    $('body').prepend($aside.append($navTopLvL.append($spacer, $listGroup.append($listContent, $listDivider, $listContent2))));
+                    this.MDCDrawer = new mdc.drawer.MDCPersistentDrawer($('#' + this.ID.toString())[0]);
+                    this.Object = $aside;
 
-        this.object = $aside;
+                    var $item1 = $('<a>', {href: "#", "class": "mdc-list-item"}).text("Send Mail");
+                    var $item1meta = $('<i>', {"class": "material-icons mdc-list-item__start-detail"}).text("send");
+                    $item1.prepend($item1meta.attr("aria-hidden", "true"));
+                    $listContent.append($item1);
+
+                    var $item2 = $('<a>', {href: "#", "class": "mdc-list-item"}).text("Drafts");
+                    var $item2meta = $('<i>', {"class": "material-icons mdc-list-item__start-detail"}).text("drafts");
+                    $item2.prepend($item1meta.attr("aria-hidden", "true"));
+                    $listContent2.append($item2);
+                    
+                    break;
+                case "Permanent Above":
+                    break;
+                case "Permanent Below":
+                    break;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Returns true if successful.
     // Returns false if unsuccessful.
     Remove() {
-        if (this.object !== null) {
-            this.object.remove();
+        if (this.Object !== null) {
+            this.Object.remove();
+            this.Object = null;
             this.MDCDrawer = null;
             return true;
         } else {
@@ -40,7 +86,7 @@ class NavDrawer {
     // Returns true if successful.
     // Returns false if unsuccessful.
     ToggleOpenClose() {
-        if (this.object !== null) {
+        if (this.Object !== null) {
             if (this.MDCDrawer.open) {
                 this.MDCDrawer.open = false;
             } else {
@@ -52,3 +98,19 @@ class NavDrawer {
         }
     }
   }
+
+class Toolbar {
+    constructor(TitleText, ID, Type) {
+        this.TitleText = "Toolbar!";
+        this.ID = ID;
+        this.Type = Type;
+        this.Object = null;
+    }
+
+    // Creates the Toolbar.
+    Create() {
+        if (this.Object === null) {
+            // Create toolbar here
+        }
+    }
+}
